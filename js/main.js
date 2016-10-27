18d8d84834b12a4a2ec23f601f79e6c157307bac
Jesse Broadfoot <Jessebroadfoot@yahoo.com>
2016-10-27 16:34:00 -0700

    Added all needed  files to project

---
+++ js/main.js
@@ -0,0 +1,15 @@
+$( ".cross" ).hide();
+$( ".menu" ).hide();
+$( ".hamburger" ).click(function() {
+$( ".menu" ).slideToggle( "fast", function() {
+$( ".hamburger" ).hide();
+$( ".cross" ).show();
+});
+});
+
+$( ".cross" ).click(function() {
+$( ".menu" ).slideToggle( "fast", function() {
+$( ".cross" ).hide();
+$( ".hamburger" ).show();
+});
+});
